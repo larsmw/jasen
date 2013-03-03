ab629b11479894974a695d378a5e5704cff9951f
Lars Nielsen <lars@lfweb.dk>
2013-03-03 14:21:10 +0100

    crawler - process handling

--- public_html/includes/classes/Logger.php
+++ public_html/includes/classes/Logger.php
@@ -56,11 +56,11 @@ class Logger {
     private function log($errorlevel, $value = '', $tag) {
 
         $datetime = date("Y-m-d H:i:s");
-        if (!file_exists($self::LOGFILENAME)) {
-            $headers = $self::HEADERS . "\n";
+        if (!file_exists(self::LOGFILENAME)) {
+            $headers = self::HEADERS . "\n";
         }
 
-        $fd = fopen($self::LOGFILENAME, "a");
+        $fd = fopen(self::LOGFILENAME, "a");
 
         if (@$headers) {
             fwrite($fd, $headers);
@@ -72,7 +72,7 @@ class Logger {
 
         $entry = array($datetime, $errorlevel, $tag, $value, $line, $file);
 
-        fputcsv($fd, $entry, $self::SEPARATOR);
+        fputcsv($fd, $entry, self::SEPARATOR);
 
         fclose($fd);
     }
