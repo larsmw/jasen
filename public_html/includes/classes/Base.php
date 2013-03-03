20b2406256c8a1f2bff3f678fcf261b15863736e
Lars Nielsen <lars@lfweb.dk>
2013-03-03 12:15:41 +0100

    Add logger class

--- public_html/includes/classes/Base.php
+++ public_html/includes/classes/Base.php
@@ -18,12 +18,12 @@ class Base {
     
     public function __construct() {
         $this->log = new Logger(ROOT.'/../logs/app.log');
-        $this->log->debug("Base is starting.");
+//        $this->log->debug("Base is starting.");
     }
     
     public function __destruct() {
 //        $this->log = new Logger(ROOT.'/../logs/app.log');
-        $this->log->info("Peak mem : ".(memory_get_peak_usage(TRUE)/1024)."kb");
+//        $this->log->info("Peak mem : ".(memory_get_peak_usage(TRUE)/1024)."kb");
     }
 }
 
