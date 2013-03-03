bc6b6d55457377087e9b406dc776396d1ec38e16
Lars Nielsen <lars@lfweb.dk>
2013-03-03 01:02:19 +0100

    .

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,20 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+/**
+Routing ideas
+
+  /MODULE/ID/METHOD
+
+  eg. 
+  /page/23/view
+  /search/the+string+to+find/list
+  /admin/user/
+  /admin/user/add
+  /crawler/control/start
+**/
+
 
 $pid = pcntl_fork(); // fork
     if ($pid < 0)
