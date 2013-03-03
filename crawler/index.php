a2b0711c950be34f5659d2bd3f9cef0907367897
Lars Nielsen <lars@lfweb.dk>
2013-03-03 13:36:34 +0100

    crawler - process handling

--- crawler/index.php
+++ crawler/index.php
@@ -1,22 +1,24 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 
 
 $pid = pcntl_fork(); // fork
-    if ($pid < 0)
+if ($pid < 0)
+    exit;
+else if ($pid) // parent
+    exit;
+else { // child
+    $sid = posix_setsid();
+
+    if ($sid < 0)
         exit;
-    else if ($pid) // parent
-        exit;
-    else { // child
-   
-        $sid = posix_setsid();
-       
-        if ($sid < 0)
-            exit;
-           
-        for($i = 0; $i <= 60; $i++) { // do something for 5 minutes
-            sleep(5);
-	    echo "$i...\n";
-        }
+
+    for ($i = 0; $i <= 6; $i++) { // do something for 5 minutes
+        sleep(5);
+        echo "pid : $pid\n";
+        echo "sid : $sid\n";
+        echo "$i...\n";
     }
+}
