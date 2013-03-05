8680d7a78c29d4fe4c0c2477d6efd72940db94dd
Lars Nielsen <lars@lfweb.dk>
2013-03-05 23:31:18 +0100

    log in

--- public_html/index.php
+++ public_html/index.php
@@ -79,22 +79,24 @@ class test extends Application {
         mysql_connect("$host", "$username", "$password") or die("cannot connect");
         mysql_select_db("$db_name") or die("cannot select DB");
 
+        $count = 0;
+        if (isset($_POST['myusername']) && isset($_POST['mypassword'])) {
 // username and password sent from form
-        $myusername = $_POST['myusername'];
-        $mypassword = $_POST['mypassword'];
+            $myusername = $_POST['myusername'];
+            $mypassword = $_POST['mypassword'];
 
 // To protect MySQL injection (more detail about MySQL injection)
-        $myusername = stripslashes($myusername);
-        $mypassword = stripslashes($mypassword);
-        $myusername = mysql_real_escape_string($myusername);
-        $mypassword = mysql_real_escape_string($mypassword);
+            $myusername = stripslashes($myusername);
+            $mypassword = stripslashes($mypassword);
+            $myusername = mysql_real_escape_string($myusername);
+            $mypassword = mysql_real_escape_string($mypassword);
 
-        $sql = "SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-        $result = mysql_query($sql);
+            $sql = "SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+            $result = mysql_query($sql);
 
 // Mysql_num_row is counting table row
-        $count = mysql_num_rows($result);
-
+            $count = mysql_num_rows($result);
+        }
 // If result matched $myusername and $mypassword, table row must be 1 row
 
         if ($count == 1) {
@@ -107,7 +109,6 @@ class test extends Application {
             echo "Wrong Username or Password";
         }
     }
-
 
     private function parse() {
       $template = file_get_contents("templates/index.html");
