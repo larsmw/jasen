0d68f6e93ee768c2efec5fe5f83b88d5d673f20d
Lars Nielsen <lars@lfweb.dk>
2013-12-14 23:25:40 +0100

    Cleanup singleton mess (DB)

--- public_html/core/classes/Database.php
+++ public_html/core/classes/Database.php
@@ -13,10 +13,24 @@ class Database {
 
     public $db;
 
-    public function __construct() {
-        
-        $this->db = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    protected static $_instance = null;
+    protected function __construct()
+    {
+        //Thou shalt not construct that which is unconstructable!
+    }
+    protected function __clone()
+    {
+        //Me not like clones! Me smash clones!
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+            $this->db = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$_instance;
     }
     
     public function fetchAssoc($sql) {
