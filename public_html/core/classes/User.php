722d232a63ddf773b51650ef3b0d770be084e329
Lars Nielsen <lars@lfweb.dk>
2014-01-19 18:08:35 +0100

    interface refactoring

--- public_html/core/classes/User.php
+++ public_html/core/classes/User.php
@@ -4,9 +4,10 @@ require_once ROOT.'/core/classes/Interfaces.php';
 require_once ROOT.'/core/classes/Base.php';
 
 
-class User extends Base implements IWebObject {
+class User extends Base implements IWebPlugin {
 
   public function onRun( $sender, $args ) {
+      echo "User object invoked...";
       //var_dump($sender); var_dump($args);
   }
 
