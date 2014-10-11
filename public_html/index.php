8b10a1fab2ac933be8216c5003a7ac2ae936b065
Lars Nielsen <lars@lfweb.dk>
2014-10-11 23:36:27 +0200

    clean up

--- public_html/index.php
+++ public_html/index.php
@@ -33,22 +33,9 @@ define("ROOT", getcwd());
 require_once 'core/core.php';
 // Death come here...
 
-require_once 'core/classes/Application.php';
-require_once 'core/classes/Crawler.php';
-require_once 'core/classes/Pager.php';
-
-class Router {
-  private $route;
-
-  public function add($r, callable $c) {
-    $this->route[$r] = $c;
-  }
-
-  public function execute() {
-    $path=isset($_SERVER['PATH_INFO'])?$_SERVER['PATH_INFO']:'/';
-    $this->r[$path]();
-  }
-}
+//require_once 'core/classes/Application.php';
+//require_once 'core/classes/Crawler.php';
+//require_once 'core/classes/Pager.php';
 
 class App implements Plugin {
     private $config;
@@ -65,7 +52,7 @@ class App implements Plugin {
 
 
 // namespaces
-use App,Crawler;
+//use App,Crawler;
 
 /**
  * Handle errors properly
@@ -73,78 +60,6 @@ use App,Crawler;
 class UnknownException extends Exception {
 
 }
-
-
-class UrlQueue extends Database {
-    public function __construct($form_id) {
-        parent::__construct();
-//        var_dump($_POST);
-    }
-
-    public function get($name) {
-        if(isset($_POST['url'])) {
-            return $_POST['url'];
-        }
-        else
-            {
-                throw new UnknownException("Missing url");
-            }
-    }
-
-    public function add($url) {
-        $sql = "INSERT INTO crawl_queue (url, added) VALUES (:url, NOW())";
-        $q = $this->db->prepare($sql);
-        $q->execute(array(':url' => $url));
-    }
-}
-
-
-/**
- * Test class to prove we are right.
- */
-class test extends \Application {
-
-    /**
-     * Start 
-     */
-    public function __construct() {
-        session_start();
-
-	$route = new Router();
-	$router->add('/crawler', [new Crawler, 'doRun']);
-	$router->add('/', 'parse');
-
-	$router->execute();
-    }
-
-    /**
-     * @args NONE
-     */
-    private function parse() {
-      $template = file_get_contents("templates/index.html");
-
-      if(empty($_COOKIE['myusername'])){
-          $login_form = file_get_contents("templates/login-form.html");
-      }
-      else {
-          $login_form = "Velkommen ".$_COOKIE['myusername']."!";
-      }
-      //$crawler = Crawler\Crawler::getInstance();
-      $template = str_replace("{%region:mainmenu}", $this->menu(), $template);
-      $template = str_replace("{%login-form}", $login_form, $template);
-      //$template = str_replace("{%url-report}", $crawler->getReport(), $template);
-
-      return $template;
-    }
-
-    public function menu() {
-        // check access rights
-        return parent::showMenu(0);
-    }
-
-}
-
-//$t = new test();
 
 $c = new Core();
 
