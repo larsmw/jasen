745f75fda4d039486933b2a5b7bb918e04ab2c20
Lars Nielsen <lars@lfweb.dk>
2014-10-11 20:53:21 +0200

    fix syntax error

--- public_html/core/classes/Crawler.php
+++ public_html/core/classes/Crawler.php
@@ -169,7 +169,7 @@ class Crawler implements \Plugin {
 
 	    $headers = get_headers($url['url'],1);
 	    var_dump($headers);
-            $contenttype = $headers['Content-Type']
+            $contenttype = $headers['Content-Type'];
 //            var_dump($contenttype);
             if($contenttype === "text/html") {
 
