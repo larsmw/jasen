184babe3cd574129fc71740269d7d63164922000
Lars Nielsen <lars@lfweb.dk>
2018-03-10 13:18:35 +0100

    dump root

--- public_html/core/classes/Database.php
+++ public_html/core/classes/Database.php
@@ -14,7 +14,7 @@ class Database extends interfaces\Singleton {
     public $db;
 
     public function __construct() {
-
+        var_dump(ROOT);
         try {        
         $this->db = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
