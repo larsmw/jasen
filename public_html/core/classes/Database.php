33c65bc57620931c0fd25d35193c467f1132fc2d
Lars Nielsen <lars@lfweb.dk>
2018-03-10 10:33:11 +0100

    ...

--- public_html/core/classes/Database.php
+++ public_html/core/classes/Database.php
@@ -11,7 +11,7 @@ define('DB_USER', 'root');
 define('DB_PASS', 'was&87Bki');
 
 
-class Database extends interfaces\Singleton {
+class App\Database extends interfaces\Singleton {
 
     public $db;
 
