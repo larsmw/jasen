aec745e80c8f902c951e4c62c105a518cbe7ebec
Lars Nielsen <lars@lfweb.dk>
2018-03-10 16:54:19 +0100

    .

--- public_html/index.php
+++ public_html/index.php
@@ -32,7 +32,7 @@ class UnknownException extends \Exception {
 /**
  * Implementation of Application class
  */
-class myApp extends App\Application {
+class myApp extends \Application {
 
     /**
      * Call constructor of parent.
