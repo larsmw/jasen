14989bf52a69c03be59772e705300d6b6f22e496
Lars Nielsen <lars@lfweb.dk>
2018-03-11 00:32:08 +0100

    layout schema prototype

--- public_html/core/classes/Url.php
+++ public_html/core/classes/Url.php
@@ -2,26 +2,20 @@
 
 namespace App;
 
-class Url extends App\Database {
-    public function __construct($form_id) {
-        parent::__construct();
-//        var_dump($_POST);
-    }
+class Url extends App\Base {
 
-    public function get($name) {
-        if(isset($_POST['url'])) {
-            return $_POST['url'];
-        }
-        else
-            {
-                throw new UnknownException("Missing url");
-            }
-    }
+  public function __construct($url) {
+    parent::__construct();
+  }
 
-    public function insert($url) {
-        $sql = "INSERT INTO crawl_queue (url, added) VALUES (:url, NOW())";
-        $q = $this->db->prepare($sql);
-        $q->execute(array(':url' => $url));
-    }
+  public function get($name) {
+        
+  }
+
+  public function insert($url) {
+    $sql = "INSERT INTO crawl_queue (url, added) VALUES (:url, NOW())";
+    $q = $this->db->prepare($sql);
+    $q->execute(array(':url' => $url));
+  }
 }
 
