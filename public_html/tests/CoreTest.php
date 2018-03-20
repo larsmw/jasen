b8405c369763dbd35be43e05e2d8e018c85a4103
Lars Nielsen <lars@lfweb.dk>
2018-03-20 23:28:31 +0100

    got path right

--- public_html/tests/CoreTest.php
+++ public_html/tests/CoreTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 define('ROOT', getcwd());
 
-include("core/classes/Application.php");
+include("public_html/core/classes/Application.php");
 
 final class CoreTest extends TestCase {
 
@@ -14,9 +14,9 @@ final class CoreTest extends TestCase {
     
     public function testCore() {
         echo getcwd();
-        //$a = new \Linkhub\myApp();
+        $a = new \Linkhub\myApp();
 
-        //$this->assertTrue(is_object($a), "a is not an object");
+        $this->assertTrue(is_object($a), "a is not an object");
     }
 
 }
