9324448f54a9ff8636063da05618799bb48877fd
Lars Nielsen <lars@lfweb.dk>
2018-08-05 21:30:28 +0200

    Adding CLI tools

--- public_html/core/classes/User.php
+++ public_html/core/classes/User.php
@@ -2,25 +2,33 @@
 
 namespace Linkhub;
 
-require_once ROOT.'/core/classes/Interfaces.php';
-require_once ROOT.'/core/classes/Base.php';
-
 class User extends Base implements \interfaces\IWebObject
 {
 
+    public function route()
+    {
+        // return array with paths and callbacks
+        $route = [
+            "user/logout" => [User=>logout]
+        ];
+    }
+
     // Handle session management for user
-    public function run($sender, $args) {
+    public function run($sender, $args)
+    {
         return null;
     }
 
-    public function onMenu(&$menu ) {
+    public function onMenu(&$menu)
+    {
         $menu[] = array( 'Forside', '<front>');
     }
 
     /**
      * Check if user login details is correct. Set cookies.
      */
-    private function validateLogin() {
+    private function validateLogin()
+    {
         $tbl_name = "users"; // Table name
 
         // number of errors
@@ -44,7 +52,6 @@ class User extends Base implements \interfaces\IWebObject
             // If result matched $myusername and $mypassword, table row must be 1 row
 
             if ($count == 1) {
-
                 // Register $myusername, $mypassword and redirect to file "login_success.php"
                 setcookie("myusername", $myusername, time() + 7200);
                 setcookie("mypassword", crypt($mypassword), time() + 7200);
